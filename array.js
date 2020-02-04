956bb6991eaf6b9ea7df7c0044813f33ca343f5f
Roger Machado <rogermachado3d@gmail.com>
2020-02-04 15:59:46 -0300

    Refact array.js to dp

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 var a = [9,3,9,3,9,7,9]
 
-function arr(array){
+/* function arr(array){
     var dict = {};
     var unpaired = 0;
     array.forEach(val=>{
@@ -13,6 +13,24 @@ function arr(array){
         }
     }
     return unpaired
+} */
+
+function arr(array, i, memo ={ }, bin_res ={}){
+    if (i > array.length -1){
+        console.log('end')
+        return bin_res['unpaired']
+    }
+    if (memo[array[i]] === undefined){
+        memo[array[i]] = 1
+        if(memo[array[i]] % 2 === 1){
+            bin_res['unpaired'] = array[i]
+        }
+    }else{
+        bin_res['paired'] = array[i]
+    }
+    console.log(array[i], memo, bin_res)
+    return arr(array, i+1, memo, bin_res)
 }
 
-console.log(arr(a))
+console.log(arr(a, 0))
+
