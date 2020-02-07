589500bad5726f9d26107ecfedb0e0560a085d5d
Roger Machado <rogermachado3d@gmail.com>
2020-02-06 23:27:00 -0300

    SQL testes - part 2

--- res_sql.sql
+++ res_sql.sql
@@ -1,8 +1,25 @@
+/* Resposta 1 */
+
+
 /* Resposta 2 */
-SELECT turma.dia_da_semana FROM turma
+SELECT DISTINCT turma.dia_da_semana FROM turma
 LEFT JOIN disciplina ON disciplina.nome = 'Matematica'
 WHERE turma.DISCIPLINA_id = disciplina.id
 
+/* Resposta 3 */
+SELECT DISTINCT * FROM aluno
+LEFT JOIN turma ON turma.ALUNO_id = aluno.Id
+LEFT JOIN disciplina ON disciplina.id = turma.DISCIPLINA_id 
+WHERE  disciplina.nome = 'Matematica' OR disciplina.nome = 'Fisica'
+GROUP by aluno.Nome
+
 /* Resposta 4 */
-SELECT * from disciplina
+SELECT DISTINCT * from disciplina
 WHERE disciplina.id NOT IN (SELECT turma.DISCIPLINA_id FROM turma)
+
+/* Resposta 5 unfinished */
+
+SELECT DISTINCT * FROM aluno
+LEFT JOIN turma ON turma.ALUNO_id = aluno.Id
+LEFT JOIN disciplina ON disciplina.id = turma.DISCIPLINA_id
+WHERE disciplina.nome = 'Matematica' AND disciplina.nome NOT LIKE 'quimica'
