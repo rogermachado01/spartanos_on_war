203434dbbf1e0d8ad43335fd70dff3066e16f5de
Roger Machado <rogermachado3d@gmail.com>
2020-02-07 00:06:15 -0300

    SQL Teste - finished.

--- res_sql.sql
+++ res_sql.sql
@@ -1,5 +1,10 @@
 /* Resposta 1 */
-
+SELECT DISTINCT * FROM aluno
+LEFT JOIN aluno_turma ON aluno_turma.aluno_id = aluno.Id
+LEFT JOIN turma ON aluno_turma.turma_id = turma.id
+LEFT JOIN professor ON professor.id = turma.PROFESSOR_id
+WHERE professor.nome = 'joao pedro'
+GROUP by aluno.Nome
 
 /* Resposta 2 */
 SELECT DISTINCT turma.dia_da_semana FROM turma
@@ -9,17 +14,20 @@ WHERE turma.DISCIPLINA_id = disciplina.id
 /* Resposta 3 */
 SELECT DISTINCT * FROM aluno
 LEFT JOIN turma ON turma.ALUNO_id = aluno.Id
-LEFT JOIN disciplina ON disciplina.id = turma.DISCIPLINA_id 
-WHERE  disciplina.nome = 'Matematica' OR disciplina.nome = 'Fisica'
+LEFT JOIN disciplina ON disciplina.id = turma.DISCIPLINA_id AND disciplina.nome = 'Matematica' 
+WHERE aluno.Nome IN (SELECT aluno.Nome FROM aluno JOIN(turma, disciplina) ON turma.ALUNO_id = aluno.id AND disciplina.id = turma.DISCIPLINA_id WHERE disciplina.nome = 'fisica')
+AND disciplina.nome IS NOT NULL
 GROUP by aluno.Nome
 
 /* Resposta 4 */
 SELECT DISTINCT * from disciplina
 WHERE disciplina.id NOT IN (SELECT turma.DISCIPLINA_id FROM turma)
 
-/* Resposta 5 unfinished */
+/* Resposta 5 */
 
 SELECT DISTINCT * FROM aluno
 LEFT JOIN turma ON turma.ALUNO_id = aluno.Id
-LEFT JOIN disciplina ON disciplina.id = turma.DISCIPLINA_id
-WHERE disciplina.nome = 'Matematica' AND disciplina.nome NOT LIKE 'quimica'
+LEFT JOIN disciplina ON disciplina.id = turma.DISCIPLINA_id AND disciplina.nome = 'Matematica'
+WHERE aluno.Nome NOT IN (SELECT aluno.Nome FROM aluno JOIN(turma, disciplina) ON turma.ALUNO_id = aluno.id AND disciplina.id = turma.DISCIPLINA_id WHERE disciplina.nome = 'quimica')
+AND disciplina.nome IS NOT NULL
+GROUP by aluno.Nome
